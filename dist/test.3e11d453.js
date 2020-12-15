a8d19f0dbe7ed52c091d3f0101fcd77d02e730df
juicejun <827387950@qq.com>
2020-12-15 19:09:52 +0800

    1

--- dist/test.3e11d453.js
+++ dist/test.3e11d453.js
@@ -184,7 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../yarn/global/node_modules/parcel/src/builtins/bundle-url.js"}],"../../../yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../../../yarn/global/node_modules/parcel/src/builtins/bundle-url.js"}],"test.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../../yarn/global/node_modules/parcel/src/builtins/css-loader.js"}],"../../../yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -212,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50502" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51429" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -389,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/test.js.map
+//# sourceMappingURL=/test.3e11d453.js.map
